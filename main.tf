bef6abcb6bfb102eb9e63228ac34c380d1e39d25
Aniruddh Chandra Dandriyal <47289626+aniruddh-23@users.noreply.github.com>
2021-10-28 15:32:16 +0530

    tf files added

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+resource "google_compute_instance" "default" {
+  name         = "compute-${var.env}"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
